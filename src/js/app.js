0bf9da2b6f0276165e3063272b935955a4a357fa
Marcus Freeman <marcusfreeman@SF-M-MFreeman.local>
2015-11-10 14:35:33 -0800

    route work

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 /** @jsx React.DOM */
 import React from 'react';
 import Router from 'react-router';
-import routes from '../routes/route';
+import Login from './components/Login';
+import routes from './routes/Route';
+
 
 
 Router.run(routes, Router.HistoryLocation, (Handler)=> {
--- src/js/routes/Route.js
+++ src/js/routes/Route.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Route} from react-router;
+import {Route} from 'react-router';
 
-import AppHandler from "./components/appHandler";
-import LoginHandler from ".components/app/loginHandler";
-import Contact from "./components/app/Contact";
+import AppHandler from "../app";
+import Login from "../components/Login";
+import Contact from "../components/Contact";
 
-let routes (
+let routes =(
   <Route name ="app" path='/' handler={AppHandler}>
     <Route name="login" path ="/login" handler={LoginHandler}/>
     <Route name="contact" path ="/contact" handler={Contact}/>
